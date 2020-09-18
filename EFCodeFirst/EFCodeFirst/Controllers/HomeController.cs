ffb94984a396c3fa62e7a2710945906e8f63841f
mahaky <yadavmahak01@gmail.com>
2020-09-18 17:59:33 +0530

    CodeFirst approach

---
+++ EFCodeFirst/EFCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using EFCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFCodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        StudentContext db = new StudentContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+    }
+}
