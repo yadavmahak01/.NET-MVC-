052bd70d5013fdd60455859b60c028f48aa50972
mahaky <yadavmahak01@gmail.com>
2020-09-28 13:33:22 +0530

    Create commit

--- EFCodeFirst/EFCodeFirst/Controllers/HomeController.cs
+++ EFCodeFirst/EFCodeFirst/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using EFCodeFirst.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace EFCodeFirst.Controllers
@@ -17,5 +14,29 @@ namespace EFCodeFirst.Controllers
             var data = db.Students.ToList();
             return View(data);
         }
+
+        /*public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if(ModelState.IsValid==true){
+            db.Students.Add(s);
+            int a= db.SaveChanges();
+            if (a > 0)
+            {
+                ViewBag.InsertMessage = "<script> alert('Data Inserted')</script>";
+                ModelState.Clear();
+            }
+            else
+            {
+                ViewBag.InsertMessage = "<script> alert('Data Not Inserted')</script>";
+            }
+        }
+            return View();
+        }*/
     }
 }
