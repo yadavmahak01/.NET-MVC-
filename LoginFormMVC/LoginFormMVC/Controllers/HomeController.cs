8c200f150ac50e54b6a0a9d239c699201ab445b8
mahaky <yadavmahak01@gmail.com>
2020-09-29 00:23:39 +0530

    LoginFormMvc

---
+++ LoginFormMVC/LoginFormMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace LoginFormMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //user comes to this page without logging thn redirect to login page
+            if (Session["username"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ LoginFormMVC/LoginFormMVC/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using LoginFormMVC.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LoginFormMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginDBEntities db = new LoginDBEntities(); 
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                var credentials = db.users.Where(model => model.username == u.username && model.password==u.password).FirstOrDefault();
+                if (credentials == null)
+                {
+                    ViewBag.ErrorMessage = "Login failed";
+                    return View();
+                }
+                else
+                {
+                    Session["username"] = u.username;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            //Destroy session
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+        public ActionResult Reset()
+        {
+            ModelState.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
